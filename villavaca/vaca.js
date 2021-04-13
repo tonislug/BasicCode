6eb2ed77bbbfb20456c622ab265747dceb931743
Tony Siracusa <79910108+tonislug@users.noreply.github.com>
2021-04-13 08:22:30 +0200

    Add files via upload

--- villavaca/vaca.js
+++ villavaca/vaca.js
@@ -1,9 +1,57 @@
-var z;
+var vv = document.getElementById("villavaca");
+var papel = vv.getContext("2d");
 
-for(var i=0; i<10; i++)
+var fondo = {
+	url: "img/tile.png",
+	cargaOK: false,
+};
+
+var vaca = {
+	url: "img/vaca.png",
+	cargaOK: false,
+};
+
+var cantidad = aleatorio(0, 10);
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+function cargarFondo()
 {
-	z = aleatorio(5, 0);
-	document.write(z + ", ");
+	fondo.cargaOK = true;
+	dibujar();
+}
+
+function cargarVacas()
+{
+	vaca.cargaOK = true;
+	dibujar();
+}
+
+function dibujar()
+{
+	if(fondo.cargaOK)
+		{
+			papel.drawImage(fondo.imagen, 0, 0);
+		}
+	
+	if(vaca.cargaOK)
+		{
+			console.log(cantidad);
+			for(var v=0; v < cantidad; v++)
+				{
+					var x = aleatorio(0, 7);
+					var y = aleatorio(0, 7);
+					var x = x * 60;
+					var y = y * 60;
+					papel.drawImage(vaca.imagen, x, y);
+				}
+		}
 }
 
 function aleatorio(min, maxi)
